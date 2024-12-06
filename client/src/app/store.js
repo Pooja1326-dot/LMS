767933544ce6936352931fa844ffff5f176d8c4b
Pooja ahire <152896138+Pooja1326-dot@users.noreply.github.com>
2024-12-06 20:30:37 +0530

    Delete client directory

--- client/src/app/store.js
+++
@@ -1,8 +0,0 @@
-import {configureStore} from "@reduxjs/toolkit"
-import rootReducer from './rootRducer'
-import { authApi } from "@/features/api/authApi"
-
-export const appstore=configureStore({
-   reducer:rootReducer,
-   middleware:(defaultMiddleware)=>defaultMiddleware().concat(authApi.middleware)
-})
